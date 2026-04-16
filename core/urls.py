6718e6a220fb5750a632d6389e351faae61acc18
jucanuro <juankanunezr@gmail.com>
2026-04-16 00:24:33 -0500

    mejora login

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', views.CoreLoginView.as_view(), name='login'),
+    path('', views.CoreLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('administracion/', views.dashboard_view_analitycs, name='administracion'),
